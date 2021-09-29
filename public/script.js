e8aa542a668a776c81292ea3d2b4a848827a4539
rainbow-bamboo <61608357+rainbow-bamboo@users.noreply.github.com>
2021-09-29 09:44:49 -0400

    encrypts and decrypts in a url  safe way

--- public/script.js
+++ public/script.js
@@ -7,26 +7,33 @@ function switchSection(id) {
     document.getElementById(id).style.display = "block"
 }
 
-function buryCapsule() {
-    const secondsSinceEpoch = Math.round(Date.now() / 1000)
-    const affirmation = document.getElementById("love-note-textarea").value
-    window.localStorage.setItem("affirmation", affirmation)
-    window.localStorage.setItem("start-time", secondsSinceEpoch)
-    init()
+function clearNote(id) {
+    document.getElementById(id).value = ""
 }
 
-function clearNote() {
+function resetNote(){
     window.localStorage.setItem("start-time", "null")
-    window.localStorage.setItem("affirmation", "")
-    document.getElementById("love-note-textarea").value = ""
+    window.localStorage.setItem("affirmation", "null")
     switchSection("writing-section")
+}
+
+function buryCapsule(id) {
+    const secondsSinceEpoch = Math.round(Date.now() / 1000)
+    const affirmation = document.getElementById(id).value
+    window.localStorage.setItem("affirmation", affirmation)
+    window.localStorage.setItem("start-time", secondsSinceEpoch)
+    clearNote(id)
+    init()
 }
 
 function remainingTime() {
     let startingTime = parseInt(window.localStorage.getItem("start-time"))
-    let endTime = startingTime + (777 * 60)
-    let secondsSinceEpoch = Math.round(Date.now() / 1000)
-    return Math.ceil((endTime - secondsSinceEpoch) / 60)
+    if(startingTime){
+        let endTime = startingTime + (777 * 60)
+        let secondsSinceEpoch = Math.round(Date.now() / 1000)
+        return Math.ceil((endTime - secondsSinceEpoch) / 60)
+    }
+    return null
 }
 
 // this function also manipulates the state
@@ -47,10 +54,25 @@ function setTimer() {
 setInterval(function() {setTimer()} , 500)
 
 
+function enc(plainText, key){
+    var b64 = CryptoJS.AES.encrypt(plainText, key).toString();
+    var e64 = CryptoJS.enc.Base64.parse(b64);
+    var eHex = e64.toString(CryptoJS.enc.Hex);
+    return eHex;
+}
+
+function dec(cipherText, key){
+   var reb64 = CryptoJS.enc.Hex.parse(cipherText);
+   var bytes = reb64.toString(CryptoJS.enc.Base64);
+   var decrypt = CryptoJS.AES.decrypt(bytes, key);
+   var plain = decrypt.toString(CryptoJS.enc.Utf8);
+   return plain;
+}
+
 function encryptTimeCapsule(message, startTime) {
     const key = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 15);
-    const encryptedMessage = CryptoJS.AES.encrypt(message, key).toString()
-    const encryptedTime = CryptoJS.AES.encrypt(startTime, key).toString()
+    const encryptedMessage = enc(message, key)
+    const encryptedTime = enc(startTime,key)
 
     return encryptedMessage.concat("&t=").concat(encryptedTime).concat("&k=").concat(key)
 }
@@ -71,12 +93,12 @@ function copyURL() {
 
 function getTimeCapsuleFromQueryParams() {
     const params = new URLSearchParams(window.location.search)
-    const message = params.get("m")
-    const time = params.get("t")
-    const key = params.get("k")
-    if (message){
+    let message = params.get("m")
+    let t = params.get("t")
+    let key = params.get("k")
+    if (message && t && key){
         return {"message" : message,
-                "startingTime": time,
+                "startingTime": t,
                 "key" : key }
     } else {
         return null
@@ -88,24 +110,25 @@ function resetURL() {
     window.location.href = bareURL;
 }
 
-function decrypt(val,key) {
-    const bytes = CryptoJS.AES.decrypt(val.toString(), key);
-    return bytes.toString(CryptoJS.enc.Utf8);
-}
 
 function storeCapsule(queryParams){
-    const message = decrypt(queryParams.message, queryParams.key)
-    const startingTime = decrypt(queryParams.startingTime, queryParams.key)
+    let m = dec(queryParams.message, queryParams.key)
+    let startingTime = dec(queryParams.startingTime, queryParams.key)
+//    alert(queryParams.message)
+//    alert(queryParams.key)
+//    alert(queryParams.startingTime)
+    alert(m)
+    alert(startingTime)
     window.localStorage.setItem("start-time", startingTime)
-    window.localStorage.setItem("affirmation", message)
+    window.localStorage.setItem("affirmation", m)
 }
 
 function init() {
     let timeLeft = remainingTime()
     let affirmation = window.localStorage.getItem("affirmation");
     let queryParams = getTimeCapsuleFromQueryParams();
-    if(queryParams) {
-        storeCapsule(getTimeCapsuleFromQueryParams())
+    if(queryParams != null) {
+        storeCapsule(queryParams)
         resetURL()
     }
 
